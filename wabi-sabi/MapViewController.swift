49363d0c6b1d47548feb40558d341b721d87067f
PrivacyPolicy <gabehwebsites@gmail.com>
2017-03-22 11:14:47 -0400

    Changed map images to shiny new ones, added two-floor navigation

--- wabi-sabi/MapViewController.swift
+++ wabi-sabi/MapViewController.swift
@@ -9,9 +9,30 @@
 import UIKit
 
 class MapViewController: UIViewController {
+    
+    var navImages: [Int:String]
+    var mapImages: [Int:String]
+    var FIRST_FLOOR: Int
+    var SECOND_FLOOR: Int
+    var floor: Int
 
-//    @IBOutlet weak var map: UIImageView!
-//
+    required init(coder aDecoder: NSCoder) {
+        FIRST_FLOOR = 0
+        SECOND_FLOOR = 1
+        floor = FIRST_FLOOR
+        navImages = [
+            FIRST_FLOOR: "button_up",
+            SECOND_FLOOR: "button_down"
+        ]
+        mapImages = [
+            FIRST_FLOOR: "ist_map_bottomfloor.png",
+            SECOND_FLOOR: "ist_map_eventfloor.png"
+        ]
+        super.init(coder: aDecoder)
+    }
+
+    @IBOutlet weak var map: UIImageView!
+
 //    @IBAction func tapMap(sender: UITapGestureRecognizer) {
 //        var touchPoint = sender.locationInView(map)
 //        var imageRatio = map.image!.size.height / map.image!.size.width
@@ -25,6 +46,25 @@ class MapViewController: UIViewController {
 //        self.presentViewController(alert, animated: true, completion: nil);
 //    }
     
+    @IBOutlet weak var floorNavButton: UIBarButtonItem!
+    @IBAction func floorNavButtonTap(sender: UIBarButtonItem) {
+        floor = (floor == FIRST_FLOOR) ? SECOND_FLOOR : FIRST_FLOOR
+        // change image to show updated floor
+        map.image = UIImage(named: mapImages[floor]!)
+        floorNavButton.image = UIImage(named: navImages[floor]!)
+        // fade in and out to make it look nicer
+        UIView.animateWithDuration(0.5, animations: {
+            self.map.alpha = 0;
+            self.map.image = UIImage(named: self.mapImages[self.floor]!);
+            self.map.alpha = 1;
+        })
+    }
+    
+    func alert(message: AnyObject) {
+        let alert = UIAlertController(title: "ALERT", message: "\(message)", preferredStyle: UIAlertControllerStyle.Alert);
+        alert.addAction(UIAlertAction(title: "KK Chickapay", style: UIAlertActionStyle.Default, handler: nil));
+        self.presentViewController(alert, animated: true, completion: nil);
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
