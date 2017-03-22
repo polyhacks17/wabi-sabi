ab646be9bfb2a4825669d5a732e8d65b01c81314
PrivacyPolicy <gabehwebsites@gmail.com>
2017-03-22 14:11:05 -0400

    Working map zooming

--- Podfile
+++ Podfile
@@ -5,6 +5,8 @@ target 'PolyHacks' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
+  pod 'ImageScrollView', :git => 'https://github.com/huynguyencong/ImageScrollView.git', :commit => '4ed28cafd55b313345fbd25d86c6bb53d4ed7072'
+
   # Pods for PolyHacks
 
   target 'PolyHacksTests' do
--- wabi-sabi/MapViewController.swift
+++ wabi-sabi/MapViewController.swift
@@ -7,11 +7,12 @@
 //
 
 import UIKit
+import ImageScrollView
 
 class MapViewController: UIViewController {
     
     var navImages: [Int:String]
-    var mapImages: [Int:String]
+    var mapImages: [Int:UIImage]
     var FIRST_FLOOR: Int
     var SECOND_FLOOR: Int
     var floor: Int
@@ -26,53 +27,41 @@ class MapViewController: UIViewController {
             FIRST_FLOOR: "button_up",
             SECOND_FLOOR: "button_down"
         ]
-        mapImages = [
-            FIRST_FLOOR: "ist_map_bottomfloor.png",
-            SECOND_FLOOR: "ist_map_eventfloor.png"
-        ]
         mapTitle = "Map"
         floorTitles = [
             FIRST_FLOOR: "First Floor",
             SECOND_FLOOR: "Commons"
         ]
+        mapImages = [
+            FIRST_FLOOR: UIImage(named: "ist_map_bottomfloor")!,
+            SECOND_FLOOR: UIImage(named: "ist_map_eventfloor")!
+        ]
         super.init(coder: aDecoder)
     }
 
-    @IBOutlet weak var map: UIImageView!
-
-//    @IBAction func tapMap(sender: UITapGestureRecognizer) {
-//        var touchPoint = sender.locationInView(map)
-//        var imageRatio = map.image!.size.height / map.image!.size.width
-//        var displayHeight = map.bounds.width * imageRatio
-//        var displayTop = map.bounds.height / 2 - 0.5 * displayHeight
-//        var left = touchPoint.x / map.bounds.width
-//        var top = (touchPoint.y - displayTop) / displayHeight
-//        
-//        let alert = UIAlertController(title: "Alert", message: "Message: (\(left), \(top)).", preferredStyle: UIAlertControllerStyle.Alert);
-//        alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.Default, handler: nil));
-//        self.presentViewController(alert, animated: true, completion: nil);
-//    }
+    @IBOutlet weak var scrollView: ImageScrollView!
     
     @IBOutlet weak var floorNavButton: UIBarButtonItem!
     @IBAction func floorNavButtonTap(sender: UIBarButtonItem) {
         floor = (floor == FIRST_FLOOR) ? SECOND_FLOOR : FIRST_FLOOR
-        // change image to show updated floor
-        map.image = UIImage(named: mapImages[floor]!)
         floorNavButton.image = UIImage(named: navImages[floor]!)
         // fade in and out to make it look nicer
-        UIView.animateWithDuration(0.25, animations: {
-            self.map.alpha = 0;
-            self.map.image = UIImage(named: self.mapImages[self.floor]!);
-            self.map.alpha = 1;
+        UIView.animateWithDuration(1/8, animations: {
+            self.scrollView.alpha = 0
+            }, completion: { (complete: Bool) in
+                self.scrollView.displayImage(self.mapImages[self.floor]!)
+                UIView.animateWithDuration(1/8, animations: {
+                    self.scrollView.alpha = 1
+                    })
         })
         changeNavTitle(self.floor)
     }
     
-    func alert(message: AnyObject) {
-        let alert = UIAlertController(title: "ALERT", message: "\(message)", preferredStyle: UIAlertControllerStyle.Alert);
-        alert.addAction(UIAlertAction(title: "KK Chickapay", style: UIAlertActionStyle.Default, handler: nil));
-        self.presentViewController(alert, animated: true, completion: nil);
-    }
+//    func alert(message: AnyObject) {
+//        let alert = UIAlertController(title: "ALERT", message: "\(message)", preferredStyle: UIAlertControllerStyle.Alert);
+//        alert.addAction(UIAlertAction(title: "KK Chickapay", style: UIAlertActionStyle.Default, handler: nil));
+//        self.presentViewController(alert, animated: true, completion: nil);
+//    }
     
     func changeNavTitle(floor: Int) {
         self.navigationItem.title = self.mapTitle + " (" + self.floorTitles[floor]! + ")"
@@ -80,8 +69,9 @@ class MapViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        changeNavTitle(self.floor)
+        // hack way to make the view defaulted to zoom = 1
+        floor = SECOND_FLOOR
+        floorNavButtonTap(UIBarButtonItem())
     }
 
     override func didReceiveMemoryWarning() {
